ebeabdbfa2e3fd12d9b5cf4f61a1fc9cf3a02a3e
Pablo Arango <pablo.arangori@hotmail.com>
2023-09-05 22:51:45 -0500

    Se agrega código para que el menú desaparezca con atributos responsive

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const openMenuAnimation = document.querySelector(".open");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+  openMenuAnimation.classList.toggle("open");
 }
