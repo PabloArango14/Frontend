bfbb9eca2c410e31d59ed7719319b697a42efabb
Pablo Arango <pablo.arangori@hotmail.com>
2023-09-05 23:21:23 -0500

    Se deshace el cambio de animación realizsado ya que no funciona correctamente y se deja el código como estaba inicialmente

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-const openMenuAnimation = document.querySelector(".open");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
-  openMenuAnimation.classList.toggle("open");
 }
