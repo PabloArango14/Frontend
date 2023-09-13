c0d64950d01f66640d9f7e4f14b0144543e194a4
Pablo Arango <pablo.arangori@hotmail.com>
2023-09-12 22:37:44 -0500

    Se realizan cambios en el menú mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  menuHamIcon.classList.toggle("inactive");
 }
